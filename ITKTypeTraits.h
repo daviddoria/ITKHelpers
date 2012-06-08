e41a6ea0ca431cf5f921103c33582be8f886a788
David Doria <daviddoria@gmail.com>
2012-06-08 16:07:36 -0400

    Add specialization for CovariantVector<unsigned char>

--- ITKTypeTraits.h
+++ ITKTypeTraits.h
@@ -25,4 +25,14 @@ struct TypeTraits<itk::VariableLengthVector<unsigned char> >
   typedef unsigned char ComponentType;
 };
 
+/** For itk::CovariantVector<unsigned char, N>, use itk::CovariantVector<float, N> as the LargerType.
+  * This is a partial specialization. */
+template <unsigned int N>
+struct TypeTraits<itk::CovariantVector<unsigned char, N> >
+{
+  typedef itk::CovariantVector<float, N> LargerType;
+  typedef float LargerComponentType;
+  typedef unsigned char ComponentType;
+};
+
 #endif
